4c54d458101505145e28ea43663751f2c5fb1d53
cesarramirez <cesar.ramirez@gtl.net>
2022-02-01 20:45:20 -0600

    UNOX-13-crear-modelos-programa

--- oxford/wsgi.py
+++ oxford/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for oxford project.
+WSGI config for _r project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oxford.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_r.settings')
 
 application = get_wsgi_application()
