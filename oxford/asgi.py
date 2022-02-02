ec46be08996453bbad7b0d491bea90e10b02b18a
cesarramirez <cesar.ramirez@gtl.net>
2022-02-01 20:52:07 -0600

    UNOX-13-crear-modelos-programa

--- oxford/asgi.py
+++ oxford/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for _r project.
+ASGI config for oxford project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_r.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oxford.settings')
 
 application = get_asgi_application()
--- oxford/urls.py
+++ oxford/urls.py
@@ -1,4 +1,4 @@
-"""_r URL Configuration
+"""oxford URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -9,7 +9,7 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another URL conf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
