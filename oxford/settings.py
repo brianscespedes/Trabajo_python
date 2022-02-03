fcc69a2f318d7a414efbbbcd79c760106305847b
cesarramirez <cesar.ramirez@gtl.net>
2022-02-02 19:32:31 -0600

    UNOX-15/modelado-en-base-a-historias

--- oxford/settings.py
+++ oxford/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'frontend',
+    'backend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ni'
 
 TIME_ZONE = 'UTC'
 
