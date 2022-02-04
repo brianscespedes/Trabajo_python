6565bf45d48c9dedb0d68a9803d8782eee61f599
cesarramirez <cesar.ramirez@gtl.net>
2022-02-03 21:18:25 -0600

    UNOX-17/create-frontend

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,9 @@
-from .views import index
+from .views import *
 from django.urls import path
 
 app_name = "frontend"
 
 urlpatterns = [
     path('', index, name="index"),
+    path('matriculas', matriculas, name="matriculas"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from backend.models import *
 
 
 def index(request):
-    return render(request, 'frontend/index.html')
+    return render(request, 'frontend/index.html', {
+        'user': request.user,
+        'courses': Course.objects.all(),
+        'students': Student.objects.all(),
+    })
+
+
+def matriculas(request):
+    return render(request, 'frontend/matriculas.html', {
+        'user': request.user,
+        'matriculas': Course.objects.all(),
+    })
+
+
+def nueva_matricula(request):
+    return render(request, 'frontend/matriculas.html', {
+        'user': request.user,
+        'matriculas': Course.objects.all(),
+    })
