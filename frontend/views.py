d595334bb6225a044fc0b31e255bf0cb965ead72
cesarramirez <cesar.ramirez@gtl.net>
2022-02-04 02:19:43 -0600

    UNOX-17/create-frontend

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from backend.models import *
+from django.forms import modelform_factory
 
 
 def index(request):
@@ -14,11 +15,12 @@ def matriculas(request):
     return render(request, 'frontend/matriculas.html', {
         'user': request.user,
         'matriculas': Course.objects.all(),
+        'form': modelform_factory(Student, fields="__all__"),
     })
 
 
 def nueva_matricula(request):
     return render(request, 'frontend/matriculas.html', {
         'user': request.user,
-        'matriculas': Course.objects.all(),
+        'form': modelform_factory(Student, fields="__all__"),
     })
